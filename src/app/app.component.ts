06f76a8eab76cd6e9341c2ef17f8e36dd8858527
martapla <martaplanella@hotmail.com>
2023-05-13 23:29:11 +0200

    calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculator';
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+
+  sum(): void {
+    this.result = this.number1 + this.number2;
+  }
+
+  restar(): void {
+    this.result = this.number1 - this.number2;
+  }
+
+  
 }
